a6faf1595d58835b8bd78f8a0c354688e1997cb9
Rogelio Yael Hernandez Jaimes <roger_libra01@hotmail.com>
2025-10-28 13:53:57 -0600

    API consume using HTTP Request/Response

--- src/main/java/com/roshercs/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/roshercs/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.roshercs.screenmatch.service.ConsumeAPI;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
 
@@ -14,8 +16,14 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		//can make the first steps at beginning
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
 		//throw new UnsupportedOperationException("Unimplemented method 'run'");
+		
+		//var auto detects the data type
+		var consumeAPI=new ConsumeAPI();
+
+		var json=consumeAPI.getData("https://www.omdbapi.com/?t=Game+of+Thrones&apikey=22d40db");
+		System.out.println(json);
 
 	}
 

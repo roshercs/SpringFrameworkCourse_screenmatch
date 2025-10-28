9e620c2f06815dba5395a1b26d54114a518978fb
Rogelio Yael Hernandez Jaimes <roger_libra01@hotmail.com>
2025-10-28 13:12:43 -0600

    First Hello World from Spring

--- src/main/java/com/roshercs/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/roshercs/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package com.roshercs.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//can make the first steps at beginning
+		System.out.println("Hello World!");
+		//throw new UnsupportedOperationException("Unimplemented method 'run'");
+
 	}
 
 }

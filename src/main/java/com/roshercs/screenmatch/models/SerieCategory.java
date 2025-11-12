80a04435f2a540e41cb63b8c32797b02a420ab9e
Rogelio Yael Hernandez Jaimes <roger_libra01@hotmail.com>
2025-11-12 14:34:51 -0600

    Spanish handler for Serie Category

--- src/main/java/com/roshercs/screenmatch/models/SerieCategory.java
+++ src/main/java/com/roshercs/screenmatch/models/SerieCategory.java
@@ -2,21 +2,30 @@ package com.roshercs.screenmatch.models;
 
 
 public enum SerieCategory {
-    ACTION("Action"),
-    ROMANCE("Romance"),
-    COMEDY("Comedy"),
-    DRAMA("Drama"),
-    CRIME("Crime");
+    ACTION("Action","Accion"),
+    ROMANCE("Romance","Romance"),
+    COMEDY("Comedy","Comedia"),
+    DRAMA("Drama","Drama"),
+    CRIME("Crime","Crimen");
 
     private String categoryOmdb;
-
-    private SerieCategory(String categoryOmdb) {
+    private String categorySpanish;
+    private SerieCategory(String categoryOmdb,String categorySpanish) {
         this.categoryOmdb = categoryOmdb;
+        this.categorySpanish=categorySpanish;
     }
     
     public static SerieCategory fromString(String text){
         for(SerieCategory category: SerieCategory.values()){
             if(category.categoryOmdb.equalsIgnoreCase(text)){
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("None category founded: "+text);
+    }
+    public static SerieCategory fromSpanish(String text){
+        for(SerieCategory category: SerieCategory.values()){
+            if(category.categorySpanish.equalsIgnoreCase(text)){
                 return category;
             }
         }
